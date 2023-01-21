b4ad4f1bb089bdc183f9664330339eac42c7ea47
Anay Bhutoria <anay@MOONSTRIKER.hitronhub.home>
2023-01-21 02:12:23 -0700

    connected server.js to mongoDB

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,35 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
+
+const mongoose = require('mongoose');
+const config = require('config');
+const db = config.get('mongoURI');
+
+
 const app = express();
+
+// Example code given by MongoDB
+
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://root:root@cluster0.pycp0uz.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
+
+
+
+const connectDB = require('./config/db');
+connectDB();
+
+app.get('/', (req, res) => res.send('Hello world!'));
+
+
+
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -10,3 +38,4 @@ app.use(express.json());
 app.listen(port, () => {
 console.log(`Server is running on port: ${port}`);
 });
+
